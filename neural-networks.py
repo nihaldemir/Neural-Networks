d596517ea892ab09c810e1627a3dba58e12883ed
nihaldemir <nihaldemiir@gmail.com>
2023-07-19 14:58:54 +0300

    Update neural-networks.py

--- neural-networks.py
+++ neural-networks.py
@@ -13,21 +13,24 @@ print("Matplotlib", matplotlib.__version__)
 # there are 3 neurons that are feeding into this neuron that we are gonna build
 # neurons are outputting some values
 
-inputs = [1.2, 5.1, 2.1]
+inputs = [1, 2, 3, 2.5]
 
 # every unique input is also going to have unique weight associated with it
 
-weights = [3.1, 2.1, 8.7]
+weights = [0.2, 0.8, -0.5, 1.0]
 
 # every unique neuron has a unique bias
 
-bias = 3
+bias = 2
 
 # 1. for a neuron: add up all the inputs times weights plus bias
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] +  inputs[3] * weights[3] + bias
 
 print(output)
 
 # P.2 Coding a Layer
+
+# inputs could either be truly input as in like values from the input layer of a neural network
+# which is just going to be a vector of values
 
