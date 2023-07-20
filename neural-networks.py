6280df27964f519f6e0bd99480149f32af0c4880
nihaldemir <nihaldemiir@gmail.com>
2023-07-20 17:35:38 +0300

    Update neural-networks.py

--- neural-networks.py
+++ neural-networks.py
@@ -44,3 +44,8 @@ weights3 = [-0.26, -0.27, 0.17, 0.87]
 bias2 = 3
 bias3 = 0.5
 
+output = [inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] +  inputs[3] * weights[3] + bias,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] +  inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] +  inputs[3] * weights3[3] + bias3]
+
+print(output)
